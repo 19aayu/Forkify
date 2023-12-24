51f56a805a30c7ed3b01caa0bd1a3086b3366ae5
19aayu <aayushisharma.sr@gmail.com>
2023-12-24 19:20:17 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '028a9893-59e8-4be1-a45d-24aaebf4761d';
+export const MODAL_CLOSE_SEC = 2.5;
